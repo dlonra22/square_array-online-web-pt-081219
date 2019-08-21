f8f44ecd3959416f12a97de11ba6ffe9218ef1fa
arnie b <arnold.bwaila@gmail.com>
2019-08-21 18:17:10 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,7 @@
 def square_array(array)
-  # your code here
+  #your code here
+  array.each do |elmnt| 
+    array[elmnt] = (array[elmnt] * array[elmnt])
+    puts " Here is element #{elmnt} "
+ end
 end
